2f28bce823871ac032142a92e4b49910ee2268f7
AnthonyEstelaF <estela.flores.antony.rober.oswaldo@iisgalvanimi.edu.it>
2021-01-07 21:44:59 +0000

    punto 5

--- serieTv/src/app/app.module.ts
+++ serieTv/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchSerietvComponent } from './search-serietv/search-serietv.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
